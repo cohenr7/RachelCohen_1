7f93564273355bc4a5da874b685b5966a3a1f2e6
Rachel Cohen <cohenr7@uw.edu>
2018-05-08 14:00:11 -0700

    filtered out those scale lengths less than 1mm because it is not accurate for them

--- scripts/myscript.R
+++ scripts/myscript.R
@@ -5,6 +5,7 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 library(dplyr)
 #this creates a new table catergorizing fish by size
 fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
+  filter(scalelength > 1)
 
 
